03eae0490b0bc5779a4f6f9a448cabeee99192dc
Brian Huston <hbriandrums@gmail.com>
2019-03-15 11:36:45 -0700

    Reduce code to reusable function

--- game.js
+++ game.js
@@ -1,37 +1,29 @@
-var table = document.createElement('table');
+var table1 = document.createElement('table');
 var table2 = document.createElement('table');
 
-for (var i = 0; i < 10; i++) {
+var createTable = function(table) {
 
-  var tableRow = document.createElement('tr');
+  for (var i = 0; i < 10; i++) {
 
-  for (var j = 0; j < 10; j++) {
-    
-    var tableData = document.createElement('td');
-    tableRow.appendChild(tableData);
+    var tableRow = document.createElement('tr');
 
+    for (var j = 0; j < 10; j++) {
+      
+      var tableData = document.createElement('td');
+      tableRow.appendChild(tableData);
+
+    }
+
+    table.appendChild(tableRow);
   }
 
-  table.appendChild(tableRow);
 }
 
-for (var i = 0; i < 10; i++) {
-
-  var tableRow = document.createElement('tr');
-
-  for (var j = 0; j < 10; j++) {
-    
-    var tableData = document.createElement('td');
-    tableRow.appendChild(tableData);
-
-  }
-
-  table2.appendChild(tableRow);
-}
+createTable(table1);
+createTable(table2);
 
 var boardOne = document.getElementById('board_one');
 var boardTwo = document.getElementById('board_two');
 
-boardOne.appendChild(table);
+boardOne.appendChild(table1);
 boardTwo.appendChild(table2);
-// document.body.appendChild(table2);
